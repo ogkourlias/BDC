0e543b9bf686a2fe47f4a452301a3ab217af1b35
ogkourlias <ogkourlias@gmail.com>
2023-06-20 13:14:19 +0200

    Finished assignment 5 partially.

---
+++ Assignment5/assignment5.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+"""python3 assignment1.py -n <aantal_cpus> [OPTIONEEL: -o <output csv file>] fastabestand1.fastq
+[fastabestand2.fastq ... fastabestandN.fastq]"""
+
+# METADATA VARIABLES
+__author__ = "Orfeas Gkourlias"
+__status__ = "WIP"
+__version__ = "0.1"
+
+# IMPORTS
+import csv
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg, col
+from pyspark.sql.types import DecimalType
+
+def create_session():
+    """Create Spark session"""
+    spark = (
+        SparkSession.builder.master("local[16]")
+        .config("spark.executor.memory", "4g")
+        .config("spark.driver.memory", "4g")
+        .getOrCreate()
+    )
+    return spark
+
+def read(spark):
+    """Run function"""
+    df = spark.read.csv("/data/datasets/EBI/interpro/refseq_scan/bacteria.nonredundant_protein.1.protein.faa.tsv", sep=r'\t', header=False)
+    return df
+
+def get_unique_ids(df):
+    ids = df.select("_c11").distinct()
+    return ids, ids.count()
+
+def get_anno_avg(df):
+    return df.agg(avg('_c8')).first()[0]
+
+def get_most_occuring(df):
+    return df.groupBy("_c13").count().sort("count", ascending=False).first()[0]
+
+def get_aa_length(df):
+    return df.select("_c2").first()[0]
+
+def get_top_10(df):
+    return df.groupBy("_c11").count().sort("count", ascending=False).take(10)
+
+def get_top_10_percentage(df):
+    with_span = df.withColumn("span", (df._c7 - df._c6) / df._c2)
+    subset = with_span.sort("span", ascending=False).select(col("_c11"), col("span"),
+                                                            col("_c12")).where(col("span") >= 0.9)
+    return subset
+
+def get_top_10_text(df):
+    return df.groupBy("_c12").count().sort("count", ascending=False).take(10)
+
+def get_bottom_10_text(df):
+    return df.groupBy("_c12").count().sort("count", ascending=True).take(10)
+
+def get_top_10_percentage_text(subset):
+    return subset.groupBy("_c12").count().sort("count", ascending=False).take(10)
+
+def get_coefficient(df):
+    """Return coefficient between protein size and number of InterPRO annotations"""
+    df = df.withColumn("num", df._c8.cast(DecimalType(10, 10)))
+    cor = df.withColumn("num2", df._c2.cast(DecimalType(10, 10))).corr("num", "num2")
+    return cor
+
+def main():
+    """Main function"""
+    ses = create_session()
+    df = read(ses)
+    id_df, id_count = get_unique_ids(df)
+    avg = get_anno_avg(df)
+    most_occuring = get_most_occuring(df)
+    aa_length = get_aa_length(df)
+    top_10 = get_top_10(df)
+    top_10_percentage_df = get_top_10_percentage(df)
+    top_10_percentage = top_10_percentage_df.take(10)
+    top_10_text = get_top_10_text(df)
+    bottom_10_text = get_bottom_10_text(df)
+    top_10_percentage_text = get_top_10_percentage_text(top_10_percentage_df)
+    cor = get_coefficient(df)
+
+    with open("output.csv", "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["1", id_count,])
+        writer.writerow(["2", avg])
+        writer.writerow(["3", most_occuring])
+        writer.writerow(["4", aa_length])
+        writer.writerow(["5", top_10])
+        writer.writerow(["6", top_10_percentage])
+        writer.writerow(["7", top_10_text])
+        writer.writerow(["8", bottom_10_text])
+        writer.writerow(["9", top_10_percentage_text])
+        writer.writerow(["10", cor])
+
+    df.explain(True)
+
+if __name__ == "__main__":
+    main()
